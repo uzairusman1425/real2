18cbac5e1580227a2792dce32e1f55e0baf41b41
uzairusman1425 <uzairusman1425@gmail.com>
2024-01-16 02:45:42 +0500

    fsfasf

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import UserContextProvider from '../context/UserContextProvider'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <UserContextProvider>
           {children}
+          <SpeedInsights />
         </UserContextProvider>
 
       </body>
